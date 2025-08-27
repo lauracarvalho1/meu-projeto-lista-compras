898c2daad7bc94cef9f6d767b6f8407c363cad9c
Laura Carvalho <laura.thalita@icloud.com>
2025-08-27 15:30:47 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment'
+  }
 });
